93c3d41a2b83f944e72b0462eb2dfdd4d64d7043
Gustavo <gustavo_gow1992@hotmail.com>
2020-11-09 15:06:04 -0600

    Integracion mp

---
+++ fallo.php
@@ -0,0 +1,7 @@
+<?php
+echo "Estado: ".$_GET['status']."<BR>";
+echo "Tipo de pago ".$_GET['payment_type']."<BR>";
+echo "External_reference ".$_GET['external_reference']."<BR>";
+echo "collection id ".$_GET['collection_id']."<BR>";
+echo "payment id ".$_GET['payment_id']."<BR>";
+?>
